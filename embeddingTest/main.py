afb1791aef26998f7f9b962ced3572b7f3361476
Pius Arhanbhunde <pjacks419@gmail.com>
2023-04-27 16:34:18 +0100

    Redoing the approach to cluster issues

--- embeddingTest/main.py
+++ embeddingTest/main.py
@@ -1,37 +1,107 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
-from utils import  clusterIssuesWithLabelInit, dict_to_list
+import json
+from utils import  (
+    clusterIssuesWithLabelInit, 
+    dict_to_list, 
+    add_the_labels_column, 
+    add_the_group_column,
+    clusterIssuesInit,
+    list_to_dict,
+    NumpyArrayEncoder,
+    preprocessSerializedJson,
+    labelIssues
+    )
+
 
 
 class IssueClusterer:
     label_embeddings = []
     
-    def __init__(self, labels, model):
+    def __init__(self, model):
         self.model = SentenceTransformer(model)
-        self.label_embeddings = self.getEmbeddings(dict_to_list(labels["label_description"]))
+        # self.label_embeddings = self.getEmbeddings(dict_to_list(labels["label_description"]))
 
     def setNewLabelSet(self, labels):
         self.label_embeddings = self.getEmbeddings(dict_to_list(labels["label_description"]))
-        pass
-
-    def getEmbeddings(self, sentences):
-        return self.model.encode(sentences)
 
 
-    def cluster(self, github_issues, threshold):
-        issue_embeddings = self.getEmbeddings(dict_to_list(github_issues["issue_title"]))
-        clusters = clusterIssuesWithLabelInit(self.label_embeddings, issue_embeddings, threshold)
-        tagged_issues = {"issue_id": {}, "label_id": {}, "label_tag": {}}
+    def getEmbeddings(self):
+        embedding_json = open("data/embeddings.json", "r")
+        embedding_json_data = embedding_json.read()
+        embedding_dict = json.loads(embedding_json_data)
+        embedding_list = dict_to_list(embedding_dict["issue_embeddings"])
+        return np.asarray(embedding_list)  
+
+
+    def createEmbeddings(self, github_issues):
+        issue_embeddings = self.model.encode(dict_to_list(github_issues["issue_title"]))
+        github_issues["issue_embeddings"] = {}
         for i in range(len(github_issues["id"])):
-            tagged_issues["issue_id"][i] = github_issues["id"][i]
-            if not (clusters[i] is None):
-                tagged_issues["label_id"][i] = labels["id"][clusters[i]]
-                tagged_issues["label_tag"][i] = labels["label_tag"][clusters[i]]
+            github_issues["issue_embeddings"][i] = issue_embeddings[i]
+        embeddings_json = open("data/embeddings.json", "w")
+        embeddings_json.write(json.dumps(github_issues, cls=NumpyArrayEncoder))
+        embeddings_json.close()
+
+
+    #to-do Catch error if file is not create or file is empty
+    def insertNewEmbeddings(self, new_github_issues):
+        new_github_embeddings =\
+            self.model.encode(dict_to_list(new_github_issues["issue_title"]))
+        _github_issues =\
+            preprocessSerializedJson("data/embeddings.json")
+        current_size = len(_github_issues["id"])
+        
+        for i in range(len(new_github_issues["id"])):
+            _github_issues["issue_embeddings"][current_size + i] =\
+                new_github_embeddings[i]
+            _github_issues["id"][current_size + i] =\
+                new_github_issues["id"][i]
+            _github_issues["issue_title"][current_size + i] =\
+                new_github_issues["issue_title"][i]
+        embeddings_json = open("data/embeddings.json", "w+")
+        embeddings_json.write(json.dumps(_github_issues, cls=NumpyArrayEncoder))
+        embeddings_json.close()
+           
+
+    def cluster(self, threshold):
+        issue_embeddings = self.getEmbeddings()
+        (clusters, _) = clusterIssuesInit(issue_embeddings, threshold)
+        grouped_issues = {"issue_id" : {}, "group_id" : {}}
+        _github_issues = preprocessSerializedJson("data/embeddings.json")
+        for i in range(len(_github_issues["id"])):
+            grouped_issues["issue_id"][i] = _github_issues["id"][i]
+            if not(clusters[i] is None):
+                grouped_issues["group_id"][i] = _github_issues["id"][clusters[i]]
+            else:
+                grouped_issues["group_id"][i] = None
+        cluster_json = open("data/cluster.json", "w+")
+        cluster_json.write(json.dumps(grouped_issues))
+        cluster_json.close()
+
+    
+    def tagIssues(self, labels, threshold):
+        _label_embeddings =\
+            self.model.encode(dict_to_list(labels["label_description"]))
+        issue_embeddings = self.getEmbeddings()
+        issueAndTags = labelIssues(_label_embeddings, issue_embeddings, threshold=threshold)
+        # print(issueAndTags)
+        _github_issues = preprocessSerializedJson("data/embeddings.json")
+        tagged_issues = {"issue_id": {}, "label_id": {}, "label_tag": {}}
+        for i in range(len(_github_issues["id"])):
+            tagged_issues["issue_id"][i] = _github_issues["id"][i]
+            if not (issueAndTags[i] == []):
+                tagged_issues["label_id"][i] = issueAndTags[i]
+                # tagged_issues["label_tag"][i] = labels["label_tag"][issueAndTags[i]]
             else:
                 tagged_issues["label_id"][i] = None
-                tagged_issues["label_tag"][i] = None
-        return tagged_issues
+                # tagged_issues["label_tag"][i] = None
+        cluster_json = open("data/tagged_issues.json", "w+")
+        cluster_json.write(json.dumps(tagged_issues))
+        cluster_json.close()
+        
+
 
 if __name__ == "__main__":
     labels = {
@@ -46,11 +116,33 @@ if __name__ == "__main__":
         "id": {0: 0, 1: 1, 2: 2},
         "issue_title" : 
             {0: "No description of the foo function in the documentation",
-             1: "Serialize function returns null",
+             1: "Serialze function returns null",
              2: "Add a deserialized function"},
     }
+
+    new_github_issues = {
+        "id": {0: 3},
+        "issue_title" : 
+            {0: "No description of the foo function in the documentation"},
+    }
+     
         
-    test_ = IssueClusterer(labels=labels, model='sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    test_ = IssueClusterer(model='sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    
+    # creates the seed embeddings 
+    # test_.createEmbeddings(github_issues=github_issues)
+
+    # test_.cluster(threshold=0.5)
+
+    # test_.insertNewEmbeddings(new_github_issues=new_github_issues)
+    # print(test_.cluster(github_issues=github_issues, threshold=0.5))# -1 implies no threshold
+
+    # print(add_the_labels_column(github_issues))
+
+    # test_.cluster(threshold=0.5)
+    # test_.tagIssues(labels, threshold=0.4)
 
 
-    print(test_.cluster(github_issues=github_issues, threshold=-1))# -1 implies no threshold
+
+    
+
--- embeddingTest/utils.py
+++ embeddingTest/utils.py
@@ -1,7 +1,18 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from numpy.linalg import norm
+import json
+from json import JSONEncoder
 
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+# decodedArrays = json.loads(encodedNumpyData)
+
+# finalNumpyArray = numpy.asarray(decodedArrays["array"])
 
 def cosineSimilarities(A,  B):
     return np.dot(A,B)/(norm(A)*norm(B))
@@ -26,6 +37,66 @@ def clusterIssuesWithLabelInit(label_embeddings, embeddings, threshold):
     return clusters
 
 
+def labelIssues(label_embeddings, embeddings, threshold):
+    label_size = len(label_embeddings)
+    emb_size = len(embeddings)
+    tagged_issues = [None]*emb_size
+    for i in range(emb_size):
+        temp = []
+        for label in range(label_size):
+            cosine_sim = cosineSimilarities(label_embeddings[label], embeddings[i])
+            if cosine_sim >= threshold:
+                temp.append((label, cosine_sim))
+        if not (temp is []):
+            tagged_issues[i] = temp
+    return tagged_issues
+
+
+# this creates/assigns clusters for every embedding
+def clusterIssuesInit(embeddings, threshold):
+    _array_size = len(embeddings)
+    clusters = [0]*_array_size
+    cluster_group = []
+
+    for i in range(_array_size):
+        temp = i
+        for group in cluster_group:
+            if group != i:
+                cosine_sim = cosineSimilarities(embeddings[i],  embeddings[group])
+                # print(cosine_sim)
+                if cosine_sim >= threshold:
+                    temp = group
+                    break
+        if temp != i:
+            clusters[i] = temp
+        else:
+            cluster_group.append(i)
+            clusters[i] = i
+
+    return clusters, cluster_group
+
+
+
+# this only creates/assigns clusters to new embeddings
+def clusterIssuesDelta(clusters, cluster_group, new_embeddings, embeddings, threshold):
+    _array_size = len(clusters)
+    _new_emb_size = len(new_embeddings)
+    for i in range(_new_emb_size):
+        temp = _array_size + i
+        for group in cluster_group:
+            cosine_sim = cosineSimilarities(new_embeddings[i],  embeddings[group])
+            if cosine_sim >= threshold:
+                temp = group
+                break
+        if temp != _array_size + i:
+            clusters.append(temp)
+        else:
+            cluster_group.append(_array_size + i)
+            clusters.append(_array_size + i)
+    return clusters, cluster_group
+
+
+
 def clusterIssuesWithLabelDelta(label_embeddings, new_embeddings, threshold):
     pass
 
@@ -37,4 +108,37 @@ def dict_to_list(data_dict):
     return data_list
 
 
+def list_to_dict(data_list):
+    data_dict = {}
+    for i in range(len(data_list)):
+        data_dict[i] = data_list[i]
+    return data_dict
 
+def add_the_group_column(issues):
+    issues["group"] = {}
+    size_ = len(issues["id"])
+    for i in range(size_):
+        issues["group"][i] = None
+    return issues
+
+
+def add_the_labels_column(issues):
+    issues["labels"] = {}
+    size_ = len(issues["id"])
+    for i in range(size_):
+        issues["labels"][i] = []
+    return issues
+
+
+# Clean way of getting a Python Dictionary from Serialised JSON
+def preprocessSerializedJson(json_file):
+    _json_h = open(json_file, "r")
+    _json_data = _json_h.read()
+    _json_h.close()
+    output = {}
+    _json_dict = json.loads(_json_data)
+    for key in _json_dict:
+        output[key] = {}
+        for index in _json_dict[key]:
+            output[key][int(index)] = _json_dict[key][index]
+    return output
