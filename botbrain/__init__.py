ec006dd804432fc19e88a2c91c1ac4f42eabb7fa
Obinna Ikeh <hikenike6@gmail.com>
2023-05-07 09:43:10 +0100

    output score

--- botbrain/__init__.py
+++ botbrain/__init__.py
@@ -62,6 +62,7 @@ class BotBrain:
 
             classifications = response.classifications
             score = classifications[0].confidence
+            print(f"{score=}")
             if score < self.confidence_threshold:
                 return ''
 
